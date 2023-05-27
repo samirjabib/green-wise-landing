5464bd323858ecea8c70c58922259e8aae4e7db1
samirdev9669 <samirdev9669@gmail.com>
2023-05-26 23:38:01 -0500

    make layout for navbar

--- src/app/common/Header/Header.tsx
+++ src/app/common/Header/Header.tsx
@@ -1,16 +1,27 @@
+import Image from "next/image";
+import GreenWiseLogo from "public/images/logo-green-wise.png";
+
 export const Header = () => {
   return (
-    <div>
-      <h1 className="border-2 border-blue-500">
-        <nav className="flex justify-between items-center">
-          <ul>
-            <li>Home</li>
-            <li>Servicios</li>
-            <li>Sobre Nosotros</li>
-            <li>Contacto</li>
-          </ul>
-        </nav>
-      </h1>
+    <div className="flex flex-row justify-between wrapper border-2 border-red-500">
+      <div>
+        <Image
+          src={GreenWiseLogo}
+          width={131}
+          height={31}
+          alt="Green Wise Logo"
+          className="h-auto w-32 object-cover"
+          priority
+        />
+      </div>
+      <nav className="">
+        <ul className="flex flex-row items-center justify-center h-full gap-x-6">
+          <li>Home</li>
+          <li>Servicios</li>
+          <li>Sobre Nosotros</li>
+          <li>Contacto</li>
+        </ul>
+      </nav>
     </div>
   );
 };
