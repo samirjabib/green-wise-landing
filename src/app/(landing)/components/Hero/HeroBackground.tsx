b7eded3d00bb021af48b97b04c8a78caa5a67f84
samirdev9669 <samirdev9669@gmail.com>
2023-05-27 00:36:06 -0500

    add

--- src/app/(landing)/components/Hero/HeroBackground.tsx
+++ src/app/(landing)/components/Hero/HeroBackground.tsx
@@ -5,8 +5,14 @@ export const HeroBackground = () => {
   return (
     <div className=" h-[90vh] relative">
       <div className="h-auto w-full">
-        <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-b from-transparent to-black opacity-50"></div>
-        <Image src={backgroundHero} alt="hero-background" fill />
+        <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-b from-transparent to-black opacity-30"></div>
+        <Image
+          src={backgroundHero}
+          alt="hero-background"
+          fill
+          className="-z-20"
+        />
+        <div className="absolute bottom-0 left-0 w-full h-full bg-gradient-to-t from-transparent to-black opacity-100"></div>
       </div>
     </div>
   );
--- src/app/common/Header/Header.tsx
+++ src/app/common/Header/Header.tsx
@@ -3,8 +3,8 @@ import GreenWiseLogo from "public/images/logo-green-wise.png";
 
 export const Header = () => {
   return (
-    <div className="flex flex-row justify-between wrapper  fixed top-0 left-0 z-50 py-4 bg-transparent transition-all duration-200 hover:bg-white">
-      <div>
+    <div className=" group flex flex-row justify-between wrapper  fixed top-0 left-0 z-50 py-4 bg-transparent transition-all duration-200 hover:bg-white">
+      <div className="">
         <Image
           src={GreenWiseLogo}
           width={131}
@@ -14,18 +14,19 @@ export const Header = () => {
           priority
         />
       </div>
+
       <nav className="">
-        <ul className="flex flex-row items-center justify-center h-full gap-x-6">
-          <li>
+        <ul className="group flex flex-row items-center justify-center h-full gap-x-6 text-white group-hover:text-black">
+          <li className="">
             <a>Home</a>
           </li>
-          <li>
+          <li className="">
             <a>Servicios</a>
           </li>
-          <li>
+          <li className="">
             <a>Sobre Nosotros</a>
           </li>
-          <li>
+          <li className="">
             <a>Contacto</a>
           </li>
         </ul>
