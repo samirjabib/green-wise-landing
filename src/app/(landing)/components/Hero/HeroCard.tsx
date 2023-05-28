9991baae9c0c0ede7fd3e02d32793916ce1ce260
samirdev9669 <samirdev9669@gmail.com>
2023-05-27 21:50:44 -0500

    add

--- src/app/(landing)/components/Hero/HeroCard.tsx
+++ src/app/(landing)/components/Hero/HeroCard.tsx
@@ -4,10 +4,10 @@ export const HeroCard = () => {
   return (
     <div className=" h-[15vh] w-full flex ">
       <div className="bg-green-500 w-full h-full flex items-center justify-center relative bottom-2 shadow-lg text-3xl">
-        <h className="text-white/80">
+        <h3 className="text-white/90">
           Abraza un futuro sostenible con servicios ambientales que marcan{" "}
-          <span className="text-white font-extrabold">la diferencia</span>
-        </h>
+          <span className="text-white/90 font-extrabold ">la diferencia</span>
+        </h3>
       </div>
     </div>
   );
