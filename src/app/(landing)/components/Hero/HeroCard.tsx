d88273b94fc97c35f63424f041aba15b22d588c7
samirdev9669 <samirdev9669@gmail.com>
2023-05-27 20:49:47 -0500

    add

--- src/app/(landing)/components/Hero/HeroCard.tsx
+++ src/app/(landing)/components/Hero/HeroCard.tsx
@@ -1,8 +1,12 @@
+import { AiOutlineDown } from "react-icons/ai";
+
 export const HeroCard = () => {
   return (
-    <div className="border-2 border-yellow-500 h-[10vh] w-full">
-      <div>
-        
+    <div className="border-2 border-yellow-500 h-[10vh] w-full grid grid-cols-6">
+      <div className="border-red-500 col-span-4">ESta es la card flotante</div>
+      <div className="border-2 border-orange-500 col-span-2 items-center justify-center flex uppercase font-extrabold tracking-tighter gap-x-2">
+        scroll
+        <AiOutlineDown size={24} className="animate-bounce mt-4" />
       </div>
     </div>
   );
--- src/app/(landing)/components/Hero/HeroTextBox.tsx
+++ src/app/(landing)/components/Hero/HeroTextBox.tsx
@@ -1,18 +1,21 @@
+import { AiOutlineWhatsApp } from "react-icons/ai";
+
 export const HeroTextBox = () => {
   return (
     <div className=" absolute  top-0 right-0 left-0 h-[90vh]  text-white flex  w-full wrapper ">
-      <div className="flex flex-col gap-y-2 mt-52">
-        <h1 className="display-title max-w-[750px] pr-16 text-start">
+      <div className="flex flex-col space-y-3 mt-52">
+        <h1 className="display-title max-w-[750px] pr-16 text-start tracking-tight">
           Transformando el mundo con soluciones ambientales: nuestro enfoque
           integral
         </h1>
-        <p className="text-base text-white/80 max-w-[510px]">
+        <p className="text-base text-white/80 max-w-[510px] tracking-tight">
           Soluciones ambientales integrales. Manejo ambiental, evaluaciones,
           permisos, gestión de riesgos y residuos. Expertos en tu servicio.
         </p>
-        <div>
-          <button className="mt-4 bg-green-500 px-6 py-2 rounded-full font-extrabold transition-all duration-200 hover:bg-green-600 shadow-md">
+        <div className="">
+          <button className="mt-4 bg-green-500 btn-main text-lg flex flex-row  gap-x-2 justify-center items-center">
             Contactanos
+            <AiOutlineWhatsApp size={25} />
           </button>
         </div>
       </div>
