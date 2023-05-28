4dadffaa13e053eb60071bbf9dc2a16eb8f798be
samirdev9669 <samirdev9669@gmail.com>
2023-05-27 22:02:06 -0500

    add

--- src/app/(landing)/components/Hero/HeroCard.tsx
+++ src/app/(landing)/components/Hero/HeroCard.tsx
@@ -4,9 +4,9 @@ export const HeroCard = () => {
   return (
     <div className=" h-[15vh] w-full flex ">
       <div className="bg-green-500 w-full h-full flex items-center justify-center relative bottom-2 shadow-lg text-3xl">
-        <h3 className="text-white/90">
+        <h3 className="text-white/">
           Abraza un futuro sostenible con servicios ambientales que marcan{" "}
-          <span className="text-white/90 font-extrabold ">la diferencia</span>
+          <span className="text-white font-extrabold ">la diferencia</span>
         </h3>
       </div>
     </div>
--- src/app/common/index.ts
+++ src/app/common/index.ts
@@ -1,3 +1,4 @@
 export * from './Footer';
 export * from './Header';
 export * from './RootProviders';
+export * from './TextSlider';
